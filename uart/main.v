4809788fceb250fa53349abe66ae40b8a6908ff7
Alex Bell <tabell@uvic.ca>
2015-04-08 20:05:03 -0700

    changed some variable names

--- uart/main.v
+++ uart/main.v
@@ -19,31 +19,37 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module main(
-    input clk,
+    input USER_CLOCK,
     input rst,
-    input serial_rx,
-    output serial_tx,
-    output [3:0] led
+    input USB_RS232_RXD,
+    output USB_RS232_TXD,
+	output GPIO_LED1,
+	output GPIO_LED2,
+	output GPIO_LED3,
+	output GPIO_LED4
     );
 	// internal
 	reg [8:0] buffer;
-	wire clk;
+	wire USER_CLOCK;
 
 	// outputs
-	reg serial_tx = 0;
-	reg [3:0] led = 4'b0100;
+	reg USB_RS232_TXD = 0;
+	reg GPIO_LED1 = 0;
+	reg GPIO_LED2 = 0;
+	reg GPIO_LED3 = 0;
+	reg GPIO_LED4 = 0;
 
 	// inputs
 	wire rst;
-	wire serial_rx;
+	wire USB_RS232_RXD;
 
 	reg [25:0] counter;
 	reg [5:0] counter_baud; // count to 43
 	reg [2:0] counter_symbol; // count to 43
 
-	always @(posedge clk) begin : proc_serial_tx
+	always @(posedge USER_CLOCK) begin : proc_USB_RS232_TXD
 		if(rst) begin
-			// serial_tx <= 0;
+			// USB_RS232_TXD <= 0;
 			// buffer <= 0;
 			counter <= 0;
 			counter_baud <= 0;
@@ -54,7 +60,7 @@ module main(
 			if (counter_baud == 43) // divide clock from 40mhz to 8*115200 bps
 			begin
 				counter_baud <= 0;
-				serial_tx <= serial_rx;
+				USB_RS232_TXD <= USB_RS232_RXD;
 			end
 			else
 				counter_baud <= counter_baud + 1;
@@ -62,8 +68,8 @@ module main(
 			if (counter == 10000000) // divide clock from 40mhz to 4 hz for flashing LEDs
 			begin
 				counter <= 0;
-				led[0] <= ~led[0];
-				led[1] <= ~led[1];
+				GPIO_LED1 <= ~GPIO_LED1;
+				GPIO_LED2 <= ~GPIO_LED2;
 			end
 			else
 				counter <= counter+1;
